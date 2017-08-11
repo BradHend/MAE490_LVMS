956538be57d624dcaf0f185bd80fa140278cc95f
brh0023 <brh0023@uah.edu>
2017-08-11 13:29:38 -0500

    added derivitive function, vertical veloc/accel graphs

--- processData.py
+++ processData.py
@@ -28,22 +28,31 @@ fig_dpi = 600
 #Ask for file name to process
 #filename = input('What is the data file to process? : ')   #ask user for data file to process
 print('The filename being processed is:',filename)                         #print what filename was input
-#fig_dpi = int(input('What dpi would you like the figures to be?: '))   #ask user for dpi value
+#fig_dpi = int(input('What dpi would you like the figures to be? (whole number): '))   #ask user for dpi value
 
 
 
 
-flightData = numpy.loadtxt(fname=filename, delimiter=',')  #get the flight data from the file
+flightData = np.loadtxt(fname=filename, delimiter=',')  #get the flight data from the file
 
 
 #function to retrieve data based on what column header is requested
 def getData(colHeaders,data_array,variable_interest): 
     j = 0
+    i = 0
     for i in range(len(colHeaders)):             #loops over entire 'colHeaders' list
         if colHeaders[i] == variable_interest:   #finds column index for requested data
                 j = i                            #hold the index value
     dataRequested = data_array[:,j]              #column of data
     return dataRequested                         #return the requested data
+
+#function to take derivitive
+def getDerivitive(data_array):
+    dataDerivitive = np.zeros((len(data_array)-1,1))
+    i = 0
+    for i in range(len(data_array)-1):             #loops over entire dataArray
+        dataDerivitive[i] = data_array[i+1] - data_array[i] #difference
+    return dataDerivitive                         #return the differenced data
 
 
 ######## Importing data section ########
@@ -88,11 +97,20 @@ if 'mx' in colHeaders:
 
 ######## Flight data characteristics section ########
 
-# velocity
 # max altitude?
+
+# vertical velocity
+velocityA = getDerivitive(altA)/getDerivitive(time_seconds)
+velocityB = getDerivitive(altB)/getDerivitive(time_seconds)
 # max velocity?
+
+# vertical acceleration
+vertAccelA = (getDerivitive(velocityA)/getDerivitive(time_seconds[1:len(time_seconds)]))/32.2
+vertAccelB = (getDerivitive(velocityB)/getDerivitive(time_seconds[1:len(time_seconds)]))/32.2
 # coast time?
+
 # drag?
+
 # #thrust?
 
 
@@ -170,9 +188,9 @@ if 'time_millis' or 'time_seconds' in colHeaders:
         plt.title('Angular Rates vs. Time') # title
         plt.ylabel('Roll [deg/s]')
         plt.xlabel('Time [s]')
-        plt.plot(time_seconds,roll, label='Roll')
-        plt.plot(time_seconds,pitch, label='Pitch')
-        plt.plot(time_seconds,yaw, label='Yaw')
+        plt.plot(time_seconds, roll, label='Roll')
+        plt.plot(time_seconds, pitch, label='Pitch')
+        plt.plot(time_seconds, yaw, label='Yaw')
         fig_accel.tight_layout()
         plt.legend()
         plt.show()
@@ -190,3 +208,34 @@ if 'time_millis' or 'time_seconds' in colHeaders:
         fig_mag.tight_layout()
         plt.show()
         plt.savefig('magneticFieldPlot.png', dpi=fig_dpi, bbox_inches='tight')
+        
+        
+        
+####### Extra Graphs #######
+    if 'velocityA' and 'velocityB' in globals():
+    # vertitcal veloctiy plot
+        fig_vertVelocity = plt.figure(8)
+        plt.title('Vertical Velocity vs. Time') # title        
+        plt.ylabel('Velocity [ft/s]')
+        plt.xlabel('Time [s]')
+        plt.plot(time_seconds[1:len(time_seconds)], velocityA, 'rs-', label='Pressure Sensor A')
+        plt.plot(time_seconds[1:len(time_seconds)], velocityB, 'bo-', label='Pressure Sensor B')
+        fig_vertVelocity.tight_layout()
+        plt.legend()
+        plt.show()
+        plt.savefig('verticalVelocityPlot.png', dpi=fig_dpi)        
+        
+        
+    if 'vertAccelA' and 'vertAccelB' in globals():
+    # vertical acceleration plot
+        fig_vertAccel = plt.figure(9)
+        plt.title('Vertical Acceleration vs. Time') # title        
+        plt.ylabel('Acceleration [g]')
+        plt.xlabel('Time [s]')
+        plt.plot(time_seconds[2:len(time_seconds)], vertAccelA, 'rs-', label='Pressure Sensor A')
+        plt.plot(time_seconds[2:len(time_seconds)], vertAccelB, 'bo-', label='Pressure Sensor B')
+        fig_vertAccel.tight_layout()
+        plt.legend()
+        plt.show()
+        plt.savefig('verticalAccelPlot.png', dpi=fig_dpi)        
+        
